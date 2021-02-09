21b4c449b5f4944f5836af1fce8d694c39794fd8
yuyu0127 <saiyuki01271210@gmail.com>
2021-02-09 18:27:51 +0900

    ボタン切り替え

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function onYouTubeIframeAPIReady() {
     {
       width: 640,
       height: 390,
-      videoId: '5qap5aO4i9A',
+      videoId: '',
       playerVars: {
         disablekb: 0,
         modestbranding: 1,
@@ -39,10 +39,8 @@ const liveIds = [
   '5qap5aO4i9A',
   'DWcJFNfaw9c'
 ]
-let index = 0
 
-function changeSource() {
-  index = (index + 1) % liveIds.length
+function changeSource(index) {
   const id = liveIds[index]
   const chatUrl = `https://www.youtube.com/live_chat?&v=${id}&embed_domain=${window.location.host.split(':')[0]}`
   ytPlayer.loadVideoById(id)
