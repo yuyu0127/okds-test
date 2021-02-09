7851a64e6b037b3a4cd224fa7594968a033443df
yuyu0127 <saiyuki01271210@gmail.com>
2021-02-09 16:33:13 +0900

    チャット削除

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ let index = 0
 function changeSource() {
   index = (index + 1) % liveIds.length
   const id = liveIds[index]
-  const chatUrl = `https://www.youtube.com/live_chat?&v=${id}&embed_domain=${window.location.host.split(':')[0]}`
+  // const chatUrl = `https://www.youtube.com/live_chat?&v=${id}&embed_domain=${window.location.host.split(':')[0]}`
   ytPlayer.loadVideoById(id)
-  document.getElementById('iframe-chat').src = chatUrl
+  // document.getElementById('iframe-chat').src = chatUrl
 }
